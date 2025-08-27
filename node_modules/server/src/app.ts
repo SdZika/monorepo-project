5ea2c34052f8b9c4465c273b7189e94c35ad5586
SdZika <markozivkovic1982@gmail.com>
2025-08-27 15:14:27 +0200

    setup server

---
+++ node_modules/server/src/app.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import factorialRoutes from "./routes/factorialRoutes.js"
+
+const app = express();
+
+app.use(cors())
+app.use(express.json())
+
+app.use("/api/factorials", factorialRoutes)
+
+app.get("/", (_req, res) => {
+  res.send("Server is running");
+});
+
+export default app;
